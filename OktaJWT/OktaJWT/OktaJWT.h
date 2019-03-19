a4b8cdad9ba03aa18f813e456a97f7df8a6c54a0
Benjamin van den Hout <benjamin@ngti.nl>
2019-03-19 11:51:42 +0100

    Add project for Carthage to build

---
+++ OktaJWT/OktaJWT/OktaJWT.h
@@ -0,0 +1,19 @@
+//
+//  OktaJWT.h
+//  OktaJWT
+//
+//  Created by Benjamin van den Hout on 19/03/2019.
+//  Copyright © 2019 Okta, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OktaJWT.
+FOUNDATION_EXPORT double OktaJWTVersionNumber;
+
+//! Project version string for OktaJWT.
+FOUNDATION_EXPORT const unsigned char OktaJWTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OktaJWT/PublicHeader.h>
+
+
